2a9061fac46133ecf42d51edb4b248d46fddbb74
youssefhassan2001 <youssefhassan2001@gmail.com>
2022-02-26 09:39:13 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,118 @@
+$('#a').on('click', function () {
+    $('#a').addClass('active').siblings().removeClass('active');
+    $('#a').addClass('block').siblings().removeClass('block');
+    $('#body').css('height', '45%');
+    $('#login').css('display', 'block');
+    $('#GraduatesAssociationServices').css('display', 'none');
+    $('#EarlyAdmission').css('display', 'none');
+    $('#FacultyServices').css('display', 'none');
+    $('#CentersServices').css('display', 'none');
+    $('#UniversityConference').css('display', 'none');
+    $('#Application').css('display', 'none');
+    $('#Blended').css('display', 'none');
+    location.href ='#body'
+    
+});
+$('#b').on('click', function () {
+    $('#b').addClass('active').siblings().removeClass('active');
+    $('#body').css('height', '25%');
+    $('#EarlyAdmission').css('display', 'block');
+    $('#GraduatesAssociationServices').css('display', 'none');
+    $('#login').css('display', 'none');
+    $('#FacultyServices').css('display', 'none');
+    $('#CentersServices').css('display', 'none');
+    $('#UniversityConference').css('display', 'none');
+    $('#Application').css('display', 'none');
+    $('#Blended').css('display', 'none');
+    location.href ='#body'
+
+});
+$('#c').on('click', function () {
+    $('#c').addClass('active').siblings().removeClass('active');
+    $('#body').css('height','30%');
+    $('#GraduatesAssociationServices').css('display', 'block');
+    $('#EarlyAdmission').css('display', 'none');
+    $('#login').css('display', 'none');
+    $('#FacultyServices').css('display', 'none');
+    $('#CentersServices').css('display', 'none');
+    $('#UniversityConference').css('display', 'none');
+    $('#Application').css('display', 'none');
+    $('#Blended').css('display', 'none');
+    location.href ='#body'
+
+});
+$('#d').on('click', function () {
+    $('#d').addClass('active').siblings().removeClass('active');
+    $('#body').css('height','30%');
+    $('#login').css('display', 'none');
+    $('#EarlyAdmission').css('display', 'none');
+    $('#GraduatesAssociationServices').css('display', 'none');
+    $('#FacultyServices').css('display', 'block');
+    $('#CentersServices').css('display', 'none');
+    $('#UniversityConference').css('display', 'none');
+    $('#Application').css('display', 'none');
+    $('#Blended').css('display', 'none');
+    location.href ='#body'
+
+    
+});
+$('#e').on('click', function () {
+    $('#e').addClass('active').siblings().removeClass('active');
+     $('#body').css('height', '35%');
+    $('#login').css('display', 'none');
+    $('#GraduatesAssociationServices').css('display', 'none');
+    $('#EarlyAdmission').css('display', 'none');
+    $('#FacultyServices').css('display', 'none');
+    $('#CentersServices').css('display', 'block');
+    $('#FacultyServices').css('display', 'none');
+    $('#UniversityConference').css('display', 'none');
+    $('#Application').css('display', 'none');
+    $('#Blended').css('display', 'none');
+    location.href ='#body'
+
+});
+$('#f').on('click', function () {
+    $('#f').addClass('active').siblings().removeClass('active');
+     $('#body').css('height', '60%');
+    $('#login').css('display', 'none');
+    $('#GraduatesAssociationServices').css('display', 'none');
+    $('#EarlyAdmission').css('display', 'none');
+    $('#FacultyServices').css('display', 'none');
+    $('#CentersServices').css('display', 'none');
+    $('#UniversityConference').css('display', 'block');
+    $('#Application').css('display', 'none');
+    $('#Blended').css('display', 'none');
+    location.href ='#body'
+
+
+});
+$('#g').on('click', function () {
+    $('#g').addClass('active').siblings().removeClass('active');
+     $('#body').css('height', '25%');
+    $('#login').css('display', 'none');
+    $('#GraduatesAssociationServices').css('display', 'none');
+    $('#EarlyAdmission').css('display', 'none');
+    $('#FacultyServices').css('display', 'none');
+    $('#CentersServices').css('display', 'none');
+    $('#UniversityConference').css('display', 'none');
+    $('#Application').css('display', 'block');
+    $('#Blended').css('display', 'none');
+    location.href ='#body'
+
+
+
+});
+$('#h').on('click', function () {
+    $('#h').addClass('active').siblings().removeClass('active');
+     $('#body').css('height', '40%');
+    $('#login').css('display', 'none');
+    $('#GraduatesAssociationServices').css('display', 'none');
+    $('#EarlyAdmission').css('display', 'none');
+    $('#FacultyServices').css('display', 'none');
+    $('#CentersServices').css('display', 'none');
+    $('#UniversityConference').css('display', 'none');
+    $('#Application').css('display', 'none');
+    $('#Blended').css('display', 'block');
+    location.href ='#body'
+
+});
